bba54182a6251c98644f764a7d9bcdf023336cc6
Manikanta <khambhampatin@gmail.com>
2020-05-27 15:54:54 +0530

    inital commit

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,20 @@
 import Nav from '../components/Nav'
+const AboutPage = (envs) => {
 
-const AboutPage = () => (
+  console.log(envs);
+  const env_value=envs;
+  return(
   <>
     <Nav />
+    
+    {env_value.envs}
     <p>Hello, I'm the about page</p>
   </>
-)
-
+  )
+}
+export async function getStaticProps() {
+  console.log(process.env.API_KEY)
+  var envs=process.env.API_KEY;
+ return {props:{envs}}
+}
 export default AboutPage
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Nav from '../components/Nav'
-
-const IndexPage = () => (
+import react from 'react';
+const IndexPage = () => {
+  const [envs,setenv]=React.useState();
+  console.log(envs);
+  React.useEffect(()=>{
+    setenv(`${process.env.EXAMPLE_KEY}`)
+  },[])
+ 
+  return(
   <>
     <Nav />
+    <div> {envs}</div>
+    
     <p>Hello, I'm the index page</p>
   </>
-)
+  )
+}
 
 export default IndexPage
