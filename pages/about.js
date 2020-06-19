5273e4da7969440ec46a0548e0aadc0a7c49811a
Manikanta <khambhampatin@gmail.com>
2020-06-19 14:15:22 +0530

    inital commit

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,19 @@
 import Nav from '../components/Nav'
-const AboutPage = (envs) => {
+const AboutPage = () => {
 
-  console.log(envs);
-  const env_value=envs;
+  const env_value=`${process.env.NEXT_PUBLIC_API_KEY}`;
   return(
   <>
     <Nav />
     
-    {env_value.envs}
+    {env_value}
     <p>Hello, I'm the about page</p>
   </>
   )
 }
-export async function getStaticProps() {
-  console.log(process.env.API_KEY)
-  var envs=process.env.API_KEY;
- return {props:{envs}}
-}
+// export async function getStaticProps() {
+//   console.log(process.env.API_KEY)
+//   var envs=process.env.API_KEY;
+//  return {props:{envs}}
+// }
 export default AboutPage
